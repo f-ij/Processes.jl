e7c330d694bc75f39d4fa3a6775c1ce2d1d4220f
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2025-02-05 12:40:38 +0100

    Extra tests

--- tests/CompFuncTest.jl
+++ tests/CompFuncTest.jl
@@ -1,25 +1,22 @@
 using Processes
-struct Fib <: ProcessAlgorithm end
 
 function Fib(args)
     (;fiblist) = args
     push!(fiblist, fiblist[end] + fiblist[end-1])
 end
 
-function Processes.prepare(::Fib, args)
+function Processes.prepare(::typeof(Fib), args)
     fiblist = Int[0, 1]
     processsizehint!(args, fiblist)
     return (;fiblist)
 end
-
-struct Luc <: ProcessAlgorithm end
 
 function Luc(args)
     (;luclist) = args
     push!(luclist, luclist[end] + luclist[end-1])
 end
 
-function Processes.prepare(::Luc, args)
+function Processes.prepare(::typeof(Luc), args)
     luclist = Int[2, 1]
     processsizehint!(args, luclist)
     return (;luclist)
@@ -30,5 +27,6 @@ FibLuc = CompositeAlgorithm( (Fib, Luc, identity), (1,1,1) )
 pcomp = Process(FibLuc; lifetime = 1000000)
 start(pcomp)
 benchmark(FibLuc, 1000000)
+
 
 
