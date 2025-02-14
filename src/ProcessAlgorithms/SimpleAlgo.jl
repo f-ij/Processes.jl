639d8603ecde9d5e957e54fdf09e0e08fcae211d
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2025-02-14 13:49:04 +0100

    Bug

--- src/ProcessAlgorithms/SimpleAlgo.jl
+++ src/ProcessAlgorithms/SimpleAlgo.jl
@@ -3,7 +3,7 @@ struct SimpleAlgo{T} <: ProcessLoopAlgorithm
 
     function SimpleAlgo(f)
         if f isa Type
-            return new{F}(f())
+            return new{f}(f())
         else
             return new{typeof(f)}(f)
         end
